7d41c237d8ad713bfdd9cc92b1fc755d301b67a0
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-24 12:06:36 -0600

    Added user and verification
    sign in sign out register and edit user

--- Gemfile
+++ Gemfile
@@ -48,6 +48,10 @@ gem "sassc-rails"
 #bootstrap 5. something
 gem "bootstrap"
 
+#devise for authentication
+gem "devise"
+
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "posts#index"
+  get 'home/about'
+  resources :posts
+
+  devise_for :users
+  devise_scope :user do
+    get '/users',           to: 'devise/registration#new'
+    get '/users/password',  to: 'devise/passwords#new'
+    get '/users/sign_out'   => 'devise/sessions#destroy'
+  end
+  
 end
