9f7df44cfc65ffffa4cb2ff6447223066e507d88
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-03-01 14:48:55 -0600

    pagy showing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
 
-  include Pagy::Backend
+  # include Pagy::Backend
 
   # GET /posts or /posts.json
   def index
